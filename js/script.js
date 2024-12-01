9a2e8128946c1acb3be3f4a671953daab241fda7
Busola Tom <busolatom@gmail.com>
2024-12-01 16:52:01 +0000

    large screen view ok,awaiting media queries

--- js/script.js
+++ js/script.js
@@ -57,3 +57,22 @@ window.addEventListener('scroll', function() {
       navbar.classList.remove('scrolled');
     }
   });
+
+  // Swiper js
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
